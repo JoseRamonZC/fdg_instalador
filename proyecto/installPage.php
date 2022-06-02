d99e8a5f9b93109745225ab317c5a4b635f42182
JoseRamonZC <44290@a.iesleopoldoqueipo.com>
2022-06-02 17:31:22 +0200

    solo queda la autorizacion de usuarios

--- proyecto/installPage.php
+++ proyecto/installPage.php
@@ -12,6 +12,7 @@
     <div class="container h-100">
         <div class="row h-100 justify-content-center align-items-center">
             <div class="col-12 bg-white rounded my-3 p-5">
+                <h1>Disfrute de su experiencia</h1>
 
             <?php include('installation.php'); ?>
 
