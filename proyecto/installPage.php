143fc965f05a67d791bc88ec1ae9fa663ba5d941
JoseRamonZC <44290@a.iesleopoldoqueipo.com>
2022-06-02 17:24:12 +0200

    poniendolo boniiito

---
+++ proyecto/installPage.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ruta de acceso</title>
+</head>
+<body>
+    <div class="container h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+            <div class="col-12 bg-white rounded my-3 p-5">
+
+            <?php include('installation.php'); ?>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
